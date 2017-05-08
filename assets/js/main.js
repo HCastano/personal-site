dd0159307522cd3942d8bf027c56688b3611352f
HCastano <castano.ha@gmail.com>
2017-05-07 23:36:00 -0400

    Add resources and demo the Masonry package

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,9 +2,23 @@
 
 $('#submit').click(function(){
 
-$.post("assets/php/send.php", $(".contact-form").serialize(),  function(response) {   
+$.post("assets/php/send.php", $(".contact-form").serialize(),  function(response) {
  $('#success').html(response);
 });
 return false;
 
+});
+
+// JS Required for Masonry Package to work
+// Code came form JFiddle found in: https://github.com/desandro/masonry/issues/405
+$(document).ready(function() {
+  var $container = $('.masonry');
+
+  $container.imagesLoaded(function() {
+    $container.masonry({
+      itemSelector: '.post-box',
+      columnWidth: '.post-box',
+      transitionDuration: 0
+    });
+  });
 });
